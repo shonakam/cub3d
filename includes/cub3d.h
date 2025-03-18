c21ba63632b6060e8ff943707b33f5f1d1fe085a
Cyanea <g3tsuzumi@gmail.com>
2025-03-19 02:36:51 +0900

    add map test

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: shonakam <shonakam@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 01:42:32 by shonakam          #+#    #+#             */
-/*   Updated: 2025/03/19 00:15:07 by shonakam         ###   ########.fr       */
+/*   Updated: 2025/03/19 02:26:03 by shonakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,7 +79,7 @@ typedef struct s_cub3d {
 void	d();
 t_cub3d		*initialize_cub(void);
 int			set_coredata(t_cub3d *cub, int fd);
-void		setup_cub(t_cub3d *cub);
+// void		setup_cub(t_cub3d *cub);
 
 void		run_cub3d(t_cub3d *cub);
 int			key_press(int key, void *param);
